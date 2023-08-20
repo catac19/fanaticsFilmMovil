5f11771fee20e092c21177391b4efc0dd6d81882
landis0 <127246196+landis0@users.noreply.github.com>
2023-08-19 21:41:45 -0400

    a
    a

--- src/app/pages/iniciosesion/iniciosesion.page.ts
+++ src/app/pages/iniciosesion/iniciosesion.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-iniciosesion',
@@ -6,8 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciosesion.page.scss'],
 })
 export class IniciosesionPage implements OnInit {
+  
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private toastController: ToastController
+    ) { }
+
+  async login() {
+    // Aquí puedes implementar la lógica de autenticación
+    // Por ahora, vamos a simular un inicio de sesión exitoso
+    if (this.username === 'vacalola' && this.password === 'vacololo') {
+      // Redirigir a la página principal o a donde quieras después del inicio de sesión
+      this.router.navigate(['/home']);
+    } else {
+      const toast = await this.toastController.create({
+        message: 'Credenciales incorrectas',
+        duration: 2000, // Duración en milisegundos
+        position: 'top' // Puedes cambiar la posición a 'bottom', 'middle', etc.
+      });
+      toast.present();
+    }
+  }
+
+
 
   ngOnInit() {
   }
