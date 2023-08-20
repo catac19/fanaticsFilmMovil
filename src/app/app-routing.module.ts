986d98da1810ab53f044af055e465b938808afcd
landis0 <127246196+landis0@users.noreply.github.com>
2023-08-19 20:50:06 -0400

    iniciosesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'iniciosesion',
+    loadChildren: () => import('./pages/iniciosesion/iniciosesion.module').then( m => m.IniciosesionPageModule)
   },
+
 ];
 
 @NgModule({
