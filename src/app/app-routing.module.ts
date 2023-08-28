0cedbf8e7674e05643d8ae50c6c003fc340e7218
Felipe Ignacio Ponce Canales <66095148+mclovin-felipe@users.noreply.github.com>
2023-08-28 11:02:53 -0400

    Cambio de ruta default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
       import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
-    path: '**',
+    path: '*',
     redirectTo: 'not-found',
     pathMatch: 'full',
   },
